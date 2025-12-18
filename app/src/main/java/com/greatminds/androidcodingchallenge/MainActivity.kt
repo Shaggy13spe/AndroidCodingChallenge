e6d2ea7f0bc912d4b1838794be106f357b324b54
Michael Morrison <michael.morrison@greatminds4u.com>
2025-12-18 12:10:46 -0500

    fixing gradle issues

--- app/src/main/java/com/greatminds/androidcodingchallenge/MainActivity.kt
+++ app/src/main/java/com/greatminds/androidcodingchallenge/MainActivity.kt
@@ -3,7 +3,7 @@ package com.greatminds.androidcodingchallenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
